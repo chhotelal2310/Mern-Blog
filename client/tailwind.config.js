683f013efb5d5bb674486373c8833efc35d1d8c8
Chhotelal Patel <chhotupatel705@gmail.com>
2024-04-11 11:29:19 +0530

    create pages and routes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+  './index.html',
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
