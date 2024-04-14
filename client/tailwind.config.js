242a83a7214a533350d915039539e10459c292c0
Chhotelal Patel <chhotupatel705@gmail.com>
2024-04-14 11:08:47 +0530

    create header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,39 @@
+
+//Old
+// const flowbite = require("flowbite-react/tailwind");
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     './index.html',
+//     './src/**/*.{js,ts,jsx,tsx}',
+//     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+//   ],
+  // theme: {
+  //   extend: {},
+  // },
+//   plugins: [
+//     require('flowbite/plugin'),
+//   ],
+// };
+
+
+
+//new
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  './index.html',
-    "./src/**/*.{js,jsx,ts,tsx}",
+    // ...
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    flowbite.content(),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  plugins: [
+    // ...
+    require('flowbite/plugin'),
+    flowbite.plugin(),
+  ],
+};
 
