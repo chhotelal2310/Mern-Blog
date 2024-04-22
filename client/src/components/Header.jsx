9fb1fd7a8587ac5a3428ca045b3c442e66af4eae
Chhotelal Patel <chhotupatel705@gmail.com>
2024-04-22 11:34:45 +0530

    Creatin the foorter section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,11 +8,11 @@ export default function Header() {
   const path = useLocation().pathname;
 
   return (
-    <Navbar className="border-b-2">
+    // bg-gray-100 text-black py-4 px-6 fixed top-0 w-full z-50
+    <Navbar className="border-b-2 ">
       <Link
         to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-      >
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           {" "}
           Chhotelal's
@@ -21,12 +21,7 @@ export default function Header() {
       </Link>
 
       <form>
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
-        />
+        <TextInput type="text" placeholder="Search..." rightIcon={AiOutlineSearch} className="hidden lg:inline" />
       </form>
 
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
